98a1eb30558e09160dec1d58103494f7e5dede38
eemntirey <eemntirey@gmail.com>
2026-09-26 17:51:04 +0300

    fix(super-admin): ajouter Starter au filtre tenants

--- super-admin/src/pages/Tenants.jsx
+++ super-admin/src/pages/Tenants.jsx
@@ -170,6 +170,7 @@ const Tenants = () => {
           <select value={planFilter} onChange={handlePlanChange} style={{ width: '180px' }}>
             <option value="">Tous plans</option>
             <option value="gratuit">Gratuit</option>
+            <option value="starter">Starter</option>
             <option value="pro">Pro</option>
             <option value="enterprise">Entreprise</option>
           </select>
